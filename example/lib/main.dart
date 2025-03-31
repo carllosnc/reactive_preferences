76aac7da2efb812700657081e0d4107ab2cffaf4
Carlos Costa <carllos.nc@gmail.com>
2025-03-31 20:34:51 -0300

    Refactor MainApp to use StatefulWidget and integrate theme toggling functionality

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,25 +2,44 @@ import 'package:example/components/decrement.dart';
 import 'package:example/components/increment.dart';
 import 'package:flutter/material.dart';
 import 'package:reactive_preferences/reactive_preferences.dart';
-import 'data/reactive_preferences.dart';
+import 'preferences/counter.dart';
+import 'preferences/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await RP.init();
   Counter.init();
+  AppTheme.init();
 
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with RPMixin {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: AppTheme.value ? Brightness.light : Brightness.dark,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Reactive Preferences'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                AppTheme.toggleTheme();
+              },
+              icon: Icon(AppTheme.value ? Icons.dark_mode : Icons.light_mode),
+            ),
+          ],
         ),
         body: Center(
           child: Column(
